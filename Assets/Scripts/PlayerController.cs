f0dda63895ed0f894284857446c4e9292438d785
s0s100 <s0s100@mail.ru>
2023-10-10 18:38:54 +0100

    Updated player controls with 3 options

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,13 +3,27 @@ using UniRx;
 
 namespace Player
 {
+    public enum MovementType
+    {
+        LimitedSpeed,
+        UnlimitedSpeed,
+        Joystick
+    }
+
     public class PlayerController : MonoBehaviour
     {
-        // Defines how close to the touch player object moves
+        [Header("For now change those values before running the game")]
+        [Tooltip("Min required distance between player position / start touch position and current touch position to move player")]
         [SerializeField] private float minTouchDistance = 0.1f;
+
+        [Tooltip("Limited Speed - Default movement to the touch position \n" +
+            "Unlimited Speed - Movement with no speed limit to the touch position \n" +
+            "Joystick - Movement which stores first touch location as joystick center")]
+        [SerializeField] private MovementType movementType = MovementType.LimitedSpeed;
 
         private PlayerData m_playerData;
         private Vector2 m_moveTouchPos = Vector2.zero;
+        private Vector2 m_startTouchPos = Vector2.zero;
 
         private void Awake()
         {
@@ -22,15 +36,40 @@ namespace Player
             SubscribeToMovement();
         }
 
-        // Called each frame to move player
         private void SubscribeToMovement()
         {
-            var periodicMovementObservator = Observable
-                .EveryUpdate()
-                .Subscribe(_ =>
-                {
-                    MovePlayer();
-                });
+            switch (movementType)
+            {
+                case MovementType.LimitedSpeed:
+                    var limitedMovementObservator = Observable
+                    .EveryUpdate()
+                    .Subscribe(_ =>
+                    {
+                        LimitedSpeedMove();
+                    });
+                    break;
+                case MovementType.UnlimitedSpeed:
+                    var unlimitedMovementObservator = Observable
+                    .EveryUpdate()
+                    .Subscribe(_ =>
+                    {
+                        UnlimitedSpeedMove();
+                    });
+                    break;
+                case MovementType.Joystick:
+                    var joystickMovementObservator = Observable
+                    .EveryUpdate()
+                    .Subscribe(_ =>
+                    {
+                        JoystickMove();
+                    });
+                    break;
+                default:
+                    Debug.LogWarning("Unknown Movement Type: " + movementType);
+                    break;
+            }
+
+
         }
 
         private void SubscribeToTouchInputManager()
@@ -40,6 +79,7 @@ namespace Player
             touchInputManager.OnTouchStart.Subscribe(touchPosition =>
             {
                 m_moveTouchPos = ScreenInfo.GetWorldTouchPos(touchPosition);
+                m_startTouchPos = m_moveTouchPos;
             });
 
             touchInputManager.OnTouchMove.Subscribe(touchPosition =>
@@ -56,39 +96,66 @@ namespace Player
         private void StopMovement()
         {
             m_moveTouchPos = transform.position;
+            m_startTouchPos = transform.position;
         }
 
-        private float CalculatDirection()
+        // xComparePosition is the point in world coordinates according to which direction is calculated
+        private float CalculatDirection(Vector2 comparePosition)
         {
-            if (m_moveTouchPos.x > transform.position.x + minTouchDistance)
+            if (m_moveTouchPos.x > comparePosition.x + minTouchDistance)
                 return 1.0f;
-            else if (m_moveTouchPos.x < transform.position.x - minTouchDistance)
+            else if (m_moveTouchPos.x < comparePosition.x - minTouchDistance)
                 return -1.0f;
             
             return 0.0f;
         }
 
-        private void MovePlayer()
+        private void LimitedSpeedMove()
         {
-            float moveDirection = CalculatDirection();
+            float moveDirection = CalculatDirection(transform.position);
             if (moveDirection == 0.0f)
                 return;
 
             float movement = moveDirection * Time.deltaTime * m_playerData.Speed;
-            Vector2 moveVector = new(movement, 0.0f);
-            if (CanMove(moveVector)) 
+            Vector3 moveVector = movement * Vector2.right;
+            Vector2 newLocation = transform.position + moveVector;
+
+            if (CanMove(newLocation)) 
                 transform.Translate(moveVector);
         }
 
-        private bool CanMove(Vector2 moveVector)
+        private void UnlimitedSpeedMove()
+        {
+            Vector2 newLocation = transform.position;
+            newLocation.x = m_moveTouchPos.x;
+
+            if (CanMove(newLocation))
+                transform.position = newLocation;
+        }
+
+        private void JoystickMove()
+        {
+            float moveDirection = CalculatDirection(m_startTouchPos);
+            if (moveDirection == 0.0f)
+                return;
+
+            float movement = moveDirection * Time.deltaTime * m_playerData.Speed;
+            Vector3 moveVector = movement * Vector2.right;
+            Vector2 newLocation = transform.position + moveVector;
+
+            if (CanMove(newLocation))
+                transform.Translate(moveVector);
+        }
+
+        private bool CanMove(Vector2 newPosition)
         {
             // Divided by 2 to have a bounary between screen and the right side of the screen equal to 2
             float playerXBoundary = m_playerData.CalculateXSize() / 2;
             float maxXScreenPos = ScreenInfo.GetMaxXPos();
             float minXScreenPos = ScreenInfo.GetMinXPos();
 
-            bool canMoveLeft = (minXScreenPos + playerXBoundary < transform.position.x) || (moveVector.x > 0.0f);
-            bool canMoveRight = (maxXScreenPos - playerXBoundary > transform.position.x) || (moveVector.x < 0.0f);
+            bool canMoveLeft = (minXScreenPos + playerXBoundary < newPosition.x);
+            bool canMoveRight = (maxXScreenPos - playerXBoundary > newPosition.x);
 
             return canMoveLeft && canMoveRight;
         }
