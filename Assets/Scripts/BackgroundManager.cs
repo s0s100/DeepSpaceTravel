93776f956b4ddf8bbeae0f26b5f2857dadc93b0b
s0s100 <s0s100@mail.ru>
2023-10-14 21:11:17 +0100

    Periodically generating clouds

--- Assets/Scripts/BackgroundManager.cs
+++ Assets/Scripts/BackgroundManager.cs
@@ -1,22 +1,59 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UniRx;
 
 public class BackgroundManager : MonoBehaviour
 {
+    [SerializeField] private float foregroundGenerationTime = 2.0f;
+    [SerializeField] private float foregroundExistanceTime = 10.0f;
+    [SerializeField] private float foregroundMovementSpeed = 0.5f;
+
     [SerializeField] private Sprite backgroundSprite;
+    [SerializeField] private Sprite[] foregroundSprites;
 
     private void Start()
     {
         GenerateBackground();
+        PeriodicallyGenerateClouds();
+    }
+
+    private void PeriodicallyGenerateClouds()
+    {
+        var timer = Observable.Interval(System.TimeSpan.FromSeconds(foregroundGenerationTime));
+        timer.Subscribe(_ =>
+        {
+            GenerateCloud();
+        }).AddTo(this);
+    }
+
+    private void GenerateCloud()
+    {
+        GameObject foregroundObject = new GameObject("Foreground Cloud");
+        SpriteRenderer foregroundRenderer = foregroundObject.AddComponent<SpriteRenderer>();
+
+        foregroundObject.transform.parent = transform;
+        foregroundObject.transform.position = Vector2.zero;
+        foregroundRenderer.sprite = RandomForegroundSprite();
+        foregroundRenderer.sortingLayerName = "Foreground";
+
+
+    }
+
+    private Sprite RandomForegroundSprite()
+    {
+        int randomNum = Random.Range(0, foregroundSprites.Length);
+        return foregroundSprites[randomNum];
+    }
+
+    private Vector2 GetRandomTopPosition()
+    {
+        //float topPosition = ScreenInfo.
+        return Vector2.zero;
     }
 
     private void GenerateBackground()
     {
         GameObject backgroundObject = new GameObject("Background Image");
         SpriteRenderer backgroundRenderer = backgroundObject.AddComponent<SpriteRenderer>();
-        
-        
         
         backgroundObject.transform.parent = transform;
         backgroundRenderer.sprite = backgroundSprite;
--- Assets/Scripts/ScreenInfo.cs
+++ Assets/Scripts/ScreenInfo.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 // Class to retreive screen information such as width, height and so on
