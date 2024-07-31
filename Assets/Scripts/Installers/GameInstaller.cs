649fdff2f7ba85dd039a7066fdce7542ea540992
Mikhail Okrugov <s0s100@mail.ru>
2024-07-31 17:21:13 +0300

    Fixing dependencies

--- Assets/Scripts/Installers/GameInstaller.cs
+++ Assets/Scripts/Installers/GameInstaller.cs
@@ -1,19 +1,21 @@
-using Config;
-using UnityEngine;
 using Zenject;
 using Core;
+using Config;
+using UnityEngine;
 
 namespace Installers
 {
     public class GameInstaller : MonoInstaller
     {
         [SerializeField] private ConfigScriptableObject config;
+        [SerializeField] private BackgroundService backgroundService;
+        [SerializeField] private GenerationService generationService;
 
         public override void InstallBindings()
         {
             Container.BindInstance(config.Data).AsSingle();
-            Container.Bind<BackgroundService>().AsSingle();
-            Container.Bind<GenerationService>().AsSingle();
+            Container.BindInstance(backgroundService).AsSingle();
+            Container.BindInstance(generationService).AsSingle();
         }
     }
 }
